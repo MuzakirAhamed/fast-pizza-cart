dfcc4d7c76225535ac88161cd29cafcdd8daa105
MuzakirAhamed <muzakirahmed97@gmail.com>
2023-08-23 19:32:29 +0530

    second commit

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,11 +4,11 @@ export default function Button({ children, type, to, onClick }) {
   const base =
     "inline-block bg-yellow-400 uppercase rounded-full tracking-wide font-semibold hover:bg-yellow-300 transition-colors duration-300";
   const styles = {
-    primary: base + "px-4 py-3 md:px-6 md:py-4",
-    small: base + "py-2 md:px-5 md:py-2.5",
+    primary: base + "py-3 px-4 sm:px-6 md:py-4 ",
+    small: base + "py-2 px-3 md:px-5 md:py-2.5",
     secondary:
       "inline-block px-4 py-3 uppercase rounded-full border-2 text-stone-400 border-stone-300 tracking-wide font-semibold hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300",
-    round: base + "px-2.5 py-1 md:px-3.5 md:py-2 text-sm hover:bg-yellow-300 transition-colors duration-300 focus:ring focus:outline-none focus:ring-offset-2 focus:ring-yellow-500 active-bg-yellow-700",
+    round: base + "py-3 px-3 md:px-3.5 md:py-2 text-sm hover:bg-yellow-300 transition-colors duration-300 focus:ring focus:outline-none focus:ring-offset-2 focus:ring-yellow-500 active-bg-yellow-700",
   };
   if (to) {
     return (
